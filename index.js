f661cd7d6e55f1a0c06646878a60d6b6c51ca583
Igor Gabriel <dev.igorsousa@gmail.com>
2023-05-21 03:11:33 -0300

    Final Route PUT tags

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const express = require('express');
 const _ = require('lodash');
 const app = express();
 const port = 2000;
+const file = './lista_etiquetas.xlsx';
 
-//Rota que retona todos os registros que estão na planilha
+/*
+*** Rota GET
+* Para mostrar os dados da planilha é bem simples, é só enviar uma requisição GET para esta Rota:"http://localhost:2000/etiquetas/".
+* Lembre-se de selecionar o method DELETE.
+*/
 app.get('/etiquetas', (req, res) => {
     const wb = xlsx.readFile(file);
     const ws = wb.Sheets["Plan1"];
@@ -32,21 +37,24 @@ app.get('/etiquetas', (req, res) => {
 
 });
 
-//Rota Delete, para deletar é necessário você passar o número da row ao qual você quer deletar como parâmetro. Ex.: '/etiquetas/4'. Você só pode deletar rows que envolvam dados, ou seja, somente as rows que envolvam as etiquetas de fato."
+/*
+*** Rota DELETE
+* Para Deletar a etiqueta é nessário passar o número da sua row, Ex.: "http://localhost:2000/etiquetas/2".
+* Lembre-se de selecionar o method DELETE.
+*/
 app.delete('/etiquetas/:index', (req, res) => {
     const wb = xlsx.readFile(file);
     const ws = wb.Sheets["Plan1"];
     const rows = xlsx.utils.sheet_to_json(ws);
     req.params.index = req.params.index - 1;
     function ec(r, c) {
-        return XLSX.utils.encode_cell({ r: r, c: c });
+        return xlsx.utils.encode_cell({ r: r, c: c });
     }
 
-    if(3 < req.params.index <= rows.length + 3) {
+    if(3 < req.params.index && req.params.index <= rows.length + 1) {
         var decode = xlsx.utils.decode_range(ws["!ref"])
         for (var reqI = req.params.index; reqI < decode.e.r; ++reqI) {
             for (var C = decode.s.c; C <= decode.e.c; ++C) {
-                console.log(C);
                 ws[xlsx.utils.encode_cell({ r: reqI, c: C })] = ws[xlsx.utils.encode_cell({ r: reqI+1, c: C })];
             }
         }
@@ -68,24 +76,50 @@ app.delete('/etiquetas/:index', (req, res) => {
 
 });
 
-//Rota Delete, para deletar é necessário você passar o número da row ao qual você quer deletar como parâmetro. Ex.: '/etiquetas/4'. Você só pode deletar rows que envolvam dados, ou seja, somente as rows que envolvam as etiquetas de fato."
+/*
+*** Rota PUT
+* Para Atualizar o valor da etiqueta é nessário passar o número da sua row, desta forma, Ex.:"http://localhost:2000/etiquetas/2".
+* Para atualizar a row é necessário passar como parâmetros query os valores que deseja atualizar, sendo esses: "tag", "name", "status", "price" e "source".
+* A passagem dos parâmetros é opcional, então escolha somente aqueles que deseja alterar, lembrando de passar na url o número correto da row que deseja alterar.
+* * Lembre-se de selecionar o method PUT.
+*/
 app.put('/etiquetas/:index', (req, res) => {
     const wb = xlsx.readFile(file);
     const ws = wb.Sheets["Plan1"];
     const rows = xlsx.utils.sheet_to_json(ws);
 
+    if(3 < req.params.index && req.params.index <= rows.length + 1) {
+        for(var reqI = 4; reqI < rows.length + 3; reqI++) {
+            if(req.params.index == reqI) {
+                const tag = req.query.tag != null ? req.query.tag : ws[`A${reqI}`].v;
+                ws[`A${reqI}`].v = tag;
+                const name = req.query.name != null ? req.query.name : ws[`B${reqI}`].v;
+                ws[`B${reqI}`].v = name;
+                const status = req.query.status != null ? req.query.status : ws[`C${reqI}`].v;
+                ws[`C${reqI}`].v = status;
+                const source = req.query.source != null ? req.query.source : ws[`D${reqI}`].v;
+                ws[`D${reqI}`].v = source;
+                const price = req.query.price != null ? req.query.price : ws[`E${reqI}`].v;
+                ws[`E${reqI}`].v = price;
+
+                xlsx.writeFile(wb, file);
+
+                res.send({
+                    'success' : true,
+                    'message': "A Row foi atualizada com sucesso"
+                });
+            }
+
+        }
+
+    } else {
         res.send({
             'success' : false,
-            'message': "A row passada não é um valor aceito. Confira novamente a planilha e verifique se está tentando deletar a correta."
+            'message': "A row passada não é um valor aceito. Confira novamente a planilha e verifique se está tentando atualizar a row correta."
         });
-
-
-
-
+    }
 });
 
 app.listen(port, () => {
     console.log(`O servidor está aberto na porta ${port}`);
 })
-
-const file = './lista_etiquetas.xlsx';
