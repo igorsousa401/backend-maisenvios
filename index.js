d34f1d36b61ad9c7d1dae066cd22e8edf264b7d5
Igor Gabriel <dev.igorsousa@gmail.com>
2023-05-21 01:20:29 -0300

    Final Route DELETE tags

--- index.js
+++ index.js
@@ -32,12 +32,55 @@ app.get('/etiquetas', (req, res) => {
 
 });
 
-//Rota que retona todos os registros que estão na planilha
-app.delete('/etiquetas/:tag', (req, res) => {
+//Rota Delete, para deletar é necessário você passar o número da row ao qual você quer deletar como parâmetro. Ex.: '/etiquetas/4'. Você só pode deletar rows que envolvam dados, ou seja, somente as rows que envolvam as etiquetas de fato."
+app.delete('/etiquetas/:index', (req, res) => {
     const wb = xlsx.readFile(file);
     const ws = wb.Sheets["Plan1"];
     const rows = xlsx.utils.sheet_to_json(ws);
-    res.send(data);
+    req.params.index = req.params.index - 1;
+    function ec(r, c) {
+        return XLSX.utils.encode_cell({ r: r, c: c });
+    }
+
+    if(3 < req.params.index <= rows.length + 3) {
+        var decode = xlsx.utils.decode_range(ws["!ref"])
+        for (var reqI = req.params.index; reqI < decode.e.r; ++reqI) {
+            for (var C = decode.s.c; C <= decode.e.c; ++C) {
+                console.log(C);
+                ws[xlsx.utils.encode_cell({ r: reqI, c: C })] = ws[xlsx.utils.encode_cell({ r: reqI+1, c: C })];
+            }
+        }
+        decode.e.r--;
+        ws['!ref'] = xlsx.utils.encode_range(decode.s, decode.e);
+        xlsx.writeFile(wb, file);
+        res.send({
+            'success' : true,
+            'message': "A Row foi deletada com sucesso"
+        });
+    } else {
+        res.send({
+            'success' : false,
+            'message': "A row passada não é um valor aceito. Confira novamente a planilha e verifique se está tentando deletar a correta."
+        });
+    }
+
+
+
+});
+
+//Rota Delete, para deletar é necessário você passar o número da row ao qual você quer deletar como parâmetro. Ex.: '/etiquetas/4'. Você só pode deletar rows que envolvam dados, ou seja, somente as rows que envolvam as etiquetas de fato."
+app.put('/etiquetas/:index', (req, res) => {
+    const wb = xlsx.readFile(file);
+    const ws = wb.Sheets["Plan1"];
+    const rows = xlsx.utils.sheet_to_json(ws);
+
+        res.send({
+            'success' : false,
+            'message': "A row passada não é um valor aceito. Confira novamente a planilha e verifique se está tentando deletar a correta."
+        });
+
+
+
 
 });
 
